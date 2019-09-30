b7024ab5b10def08346b4b0d918b2f0176651050
Mary Kanu <maryikanu@gmail.com>
2019-09-30 10:54:15 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -65,12 +65,15 @@ def adding_matz
           :languages => ["C"]
         }
      }
-programmer_hash = {grace_hopper:"COBOL,FORTRAN", alan_kay:"Smalltalk,LISP", dennis_ritchie:"Unix,C"
+programmer_hash = {grace_hopper:"COBOL,FORTRAN", alan_kay:"Smalltalk,LISP", dennis_ritchie:"Unix,C"}
 
-programmer_hash [:yukihiro_matsumoto => {
+   programmer_hash:yukihiro_matsumoto => {
    :known_for => "Ruby",
    :languages => ["LISP", "C"]]
-    
+   
+   puts programmer_hash
+   }
+}
 end
 
 def changing_alan
