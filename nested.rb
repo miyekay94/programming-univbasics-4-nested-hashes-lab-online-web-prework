7d63298222692c82263a2bc5468e1ed7169e5924
Mary Kanu <maryikanu@gmail.com>
2019-09-30 10:59:18 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -1,4 +1,29 @@
+describe "nested hash" do
+  describe "hopper" do
+    #it "operates on the programmer_hash and returns the value of the :grace_hopper key" do
+expect(hopper).to eq({:known_for => "COBOL", :languages => ["COBOL", "FORTRAN"]})
 
+    
+  end
+
+  describe "alan_kay_is_known_for" do
+    #it "operates on the programmer_hash and returns the value of what Alan Kay is known for" do
+
+expect(alan_kay_is_known_for).to eq("Object Orientation")
+    
+  end
+
+  describe "dennis_ritchies_language" do
+    it "operates on the programmer_hash and returns the value of Dennis Ritchie's language as a string" do
+      expect(dennis_ritchies_language).to eq("C")
+    end
+  end
+
+  describe "adding_matz" do 
+    it "operates on the programmer_hash and adds a key/value pair to the top level of the hash, returning the newly-added-to hash" do
+      expect(adding_matz.keys).to include(:yukihiro_matsumoto)
+    end
+  end
 def hopper
 	programmer_hash = 
  		{
